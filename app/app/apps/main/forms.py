3a6dad736f43b39f7b855c3560c0dc93df3fa0c2
lipskydan <lipsky@knu.ua>
2021-07-02 22:53:59 +0300

    now add name auto

--- app/app/apps/main/forms.py
+++ app/app/apps/main/forms.py
@@ -4,13 +4,13 @@ from django.forms import widgets
 
 
 class OurImgForm(forms.ModelForm):
-    name = forms.CharField(label='Ім\'я фото', max_length=30, required=True, help_text='Обов’язково',
-                           localize=True,
-                           widget=widgets.TextInput(attrs={'size': 1, 'class': 'form-control'}))
+    # name = forms.CharField(label='Ім\'я фото', max_length=30, required=True, help_text='Обов’язково',
+    #                        localize=True,
+    #                        widget=widgets.TextInput(attrs={'size': 1, 'class': 'form-control'}))
 
     img = forms.ImageField(label=('Фото'), required=False, error_messages={'invalid': ("Image files only")},
                            widget=widgets.FileInput(attrs={'size': 1, 'class': 'form-control'}))
 
     class Meta:
         model = OurImg
-        fields = ['name', 'img']
+        fields = ('img',)
